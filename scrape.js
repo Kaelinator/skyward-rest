c116fdb800d70b86f885174ca0fa76bd13ec4335
Kaelinator <kirkkael@gmail.com>
2017-10-10 14:44:30 -0500

    Scraper now properly waits

--- scrape.js
+++ scrape.js
@@ -1,16 +1,27 @@
 const {Builder, By, Key, until} = require('selenium-webdriver')
 
+
 const scrape = module.exports = (url, sId, pass) => {
-
+  
   /* TODO: Scrape */
-
+  
   const driver = new Builder()
-      .forBrowser('chrome')
-      .build()
+    .forBrowser('chrome')
+    .build()
+
+  const waitForId = (id) => driver.wait(until.elementLocated(By.id(id)), 1000)
+  const inputCredentials = () => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
+  // const getHandles = () => driver.getAllWindowHandles()
+  const switchToPopup = (w) => driver.switchTo(w[w.length - 1])
+  
 
   driver.get(url)
-    .then(() => driver.wait(until.titleIs('Login - Powered by Skyward'), 1000))
-    .then(() => driver.findElement(By.name('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN))
+    .then(waitForId('login'))
+    .then(waitForId('password'))
+    .then(inputCredentials)
+    .then(driver.getAllWindowHandles().then(switchToPopup))
+    .then(() => driver.findElement(By.id('login')).sendKeys('Hi there'))
+    // .then(() => driver.findElement(By.id('PostText')).sendKeys('Hi there'))
   
   
   // driver.quit()
