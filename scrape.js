7b82279f334d907456a6e38ebabbc3a9d9424c2f
Kaelinator <kirkkael@gmail.com>
2017-10-11 14:45:22 -0500

    Correctlly sleeping

--- scrape.js
+++ scrape.js
@@ -12,16 +12,18 @@ const scrape = module.exports = (url, sId, pass) => {
   const waitForId = (id) => driver.wait(until.elementLocated(By.id(id)), 1000)
   const inputCredentials = () => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
   // const getHandles = () => driver.getAllWindowHandles()
-  const switchToPopup = (w) => driver.switchTo(w[w.length - 1])
+  const switchToPopup = (w) => driver.switchTo(w[w.length - 2])
   
 
   driver.get(url)
     .then(waitForId('login'))
     .then(waitForId('password'))
     .then(inputCredentials)
+    .then(() => { driver.sleep(5000) })
+    .then(() => { driver.getAllWindowHandles().then(console.log) })
     .then(driver.getAllWindowHandles().then(switchToPopup))
-    .then(() => driver.findElement(By.id('login')).sendKeys('Hi there'))
-    // .then(() => driver.findElement(By.id('PostText')).sendKeys('Hi there'))
+    // .then(() => driver.findElement(By.id('login')).sendKeys('Hi there'))
+    .then(() => driver.findElement(By.id('PostText')).sendKeys('Hi there'))
   
   
   // driver.quit()
