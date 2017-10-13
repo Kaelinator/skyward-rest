6528848f40125c7aa0460394b6688a96c01e226d
Kaelinator <kirkkael@gmail.com>
2017-10-13 11:50:30 -0500

    Every assignment now available

--- scrape.js
+++ scrape.js
@@ -1,30 +1,42 @@
-const {Builder, By, Key, until} = require('selenium-webdriver')
+const { Builder, By, Key, until } = require('selenium-webdriver')
 
 
 const scrape = module.exports = (url, sId, pass) => {
-  
-  /* TODO: Scrape */
   
   const driver = new Builder()
     .forBrowser('chrome')
     .build()
 
-  const waitForId = (id) => driver.wait(until.elementLocated(By.id(id)), 1000)
-  const inputCredentials = () => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
-  // const getHandles = () => driver.getAllWindowHandles()
-  const switchToPopup = (w) => driver.switchTo(w[w.length - 2])
-  
+  const waitForId        = (id) => driver.wait(until.elementLocated(By.id(id)), 1000)
+  const inputCredentials = ()   => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
+  const getHandles       = ()   => driver.getAllWindowHandles()
+  const swap             = (w)  => driver.getAllWindowHandles().then((w) => driver.switchTo().window(w[w.length - 1]))
+  const wait             = (x)  => driver.sleep(x)
 
-  driver.get(url)
+  const access = driver.get(url)
     .then(waitForId('login'))
     .then(waitForId('password'))
+    .then(wait(500))
     .then(inputCredentials)
-    .then(() => { driver.sleep(5000) })
-    .then(() => { driver.getAllWindowHandles().then(console.log) })
-    .then(driver.getAllWindowHandles().then(switchToPopup))
-    // .then(() => driver.findElement(By.id('login')).sendKeys('Hi there'))
-    .then(() => driver.findElement(By.id('PostText')).sendKeys('Hi there'))
+    .then(wait(1000))
+    .then(swap)
+    
+  const gradebook = access
+    .then(() => driver.findElement(By.xpath('//*[@data-nav="sfgradebook001.w"]')).click())
+    .then(() => driver.findElement(By.id('classGradesOptions_74872_004')).click())
+    .then(() => {
+      element = driver.findElement(By.id('showAssignmentsLink_74872_004'))
+      element.getAttribute('data-show').then((show) => {
+        if (show == 'no') {
+          element.click()
+        }
+      })
+    })
+    .then(() => {
+      const grades = driver.findElements(By.css('a[data-title="Assignment Details"]'))
+      grades.then((x) => {
+        x.forEach((y) => y.getAttribute('innerHTML').then(console.log))
+      })
+    })
   
-  
-  // driver.quit()
 }
