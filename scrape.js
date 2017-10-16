e465a932cf6d52330eecaab62c4793c90494540d
Kaelinator <kirkkael@gmail.com>
2017-10-15 22:00:55 -0500

    Beginning the structure of returning data

--- scrape.js
+++ scrape.js
@@ -2,60 +2,53 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const Promise = require('bluebird')
 
 const scrape = module.exports = (url, sId, pass) => {
-  
+  const echo             = lbl  => console.log(lbl) || lbl
+  const waitForId        = id   => driver.wait(until.elementIsVisible(driver.findElement(By.id(id))), 1000)
+  const inputCredentials = ()   => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
+  const getHandles       = ()   => driver.getAllWindowHandles()
+  const swap             = w    => driver.getAllWindowHandles().then((w) => driver.switchTo().window(w[w.length - 1]))
+  const wait             = x    => driver.sleep(x)
+
   const driver = new Builder()
     .forBrowser('chrome')
     .build()
-
-  return new Promise((resolve, reject) => {
-
-    const waitForId        = (id) => driver.wait(until.elementIsVisible(driver.findElement(By.id(id))), 1000)
-    const inputCredentials = ()   => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
-    const getHandles       = ()   => driver.getAllWindowHandles()
-    const swap             = (w)  => driver.getAllWindowHandles().then((w) => driver.switchTo().window(w[w.length - 1]))
-    const wait             = (x)  => driver.sleep(x)
     
-    let classes = []
+  return driver.get(url)
+    .then(waitForId('login'))
+    .then(waitForId('password'))
+    .then(inputCredentials)
+    .then(wait(1000))
+    .then(swap)
+    .then(() => driver.findElement(By.xpath('//a[@data-nav="sfgradebook001.w"]')).click())
+    .then(() => driver.findElements(By.name('showGradeInfo')))
 
-    const access = driver.get(url)
-      .then(waitForId('login'))
-      .then(waitForId('password'))
-      .then(inputCredentials)
-      .then(wait(1000))
-      .then(swap)
-    
-    const gradebook = access
-      .then(() => driver.findElement(By.xpath('//*[@data-nav="sfgradebook001.w"]')).click())
-      .then(async () => {
+    .then(grades => {
 
-        let grades = await driver.findElements(By.name('showGradeInfo'))
-        // grades = [grades[0]]
-        Promise.each(grades, async (g) => {
 
-          const score = await g.getAttribute('innerHTML')
-          const bkt = await g.getAttribute('data-bkt')
+      return Promise
+        .resolve(grades)
+        .reduce((data, grade) => grade.getAttribute('data-bkt')
+            .then(bkt => {
+              if (!data.hasOwnProperty(bkt))
+                data[bkt] = []
 
-          await driver.executeScript('arguments[0].scrollIntoView()', g)
-            .then(() => { console.log('scrolling') })
-          /* sometimes this doesn't actually scroll... */
+              data[bkt].push(grade)
 
-          await g.click()
-            .then(() => { console.log('awaiting heading')
-              driver.wait(until.elementLocated(By.className('gb_heading'))) })
-            .then(() => { console.log('grabbing heading')
-              driver.findElement(By.className('gb_heading')).then((x) => x.getAttribute('innerHTML').then(console.log)) })
-            .then(() => { driver.findElement(By.css('.sf_gridTableWrap tbody td')) })
-            .then(() => { console.log('awaiting exit button')
-              driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))) })
-            .then(() => { console.log('clicking exit button')
-              driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')).click() })
-            .then(() => { console.log('awaiting exit')
-              driver.wait(until.elementIsNotVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))) })
-
-        })
-        
-      })
-    
-    resolve(classes)
-  })
+              return data
+            }), {})
+        .then((data) => Object.keys(data)
+            .reduce((obj, bkt) => {
+              obj[bkt].map((grade) => {
+                
+                return driver.executeScript('arguments[0].scrollIntoView()', grade)
+                  .then(() => grade.click())
+                  .then(() => driver.wait(until.elementLocated(By.className('gb_heading'))))
+                  .then(() => driver.findElement(By.className('gb_heading')).then((x) => x.getAttribute('innerHTML').then(console.log)))
+                  .then(() => driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
+                  .then(() => driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')).click())
+                  .then(() => driver.wait(until.elementIsNotVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
+                })
+              return obj
+            }, data))
+    })
 }
