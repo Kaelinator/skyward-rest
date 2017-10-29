66f4a2312bc185ad9e30fba1a8b50fff4bc576aa
Kaelinator <kirkkael@gmail.com>
2017-10-29 13:00:01 -0500

    Restructuring

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const Promise = require('bluebird')
 
 const skyward = module.exports = 
     (url) =>
-        (sId, pass) => scrape(url, sId, pass)
+        (sId, pass, target) => scrape(url, sId, pass, target)
 
-skyward('https://skyward.kleinisd.net/')('s531758', 'ASDF;lkj')
-    .then(Promise.props)
-    .then((x) => x['TERM 6'][0])
+
+skyward('https://skyward.kleinisd.net/')('s531758', 'ASDF;lkj', 'S1')
+    // .then(Promise.props)
+    .then(Promise.all)
     .then(console.log)
--- scrape.js
+++ scrape.js
@@ -1,7 +1,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const Promise = require('bluebird')
 
-const scrape = module.exports = (url, sId, pass) => {
+const scrape = module.exports = (url, sId, pass, target) => {
   const waitForId        = id   => driver.wait(until.elementIsVisible(driver.findElement(By.id(id))), 1000)
   const inputCredentials = ()   => driver.findElement(By.id('login')).sendKeys(sId, Key.TAB, pass, Key.RETURN)
   const getHandles       = ()   => driver.getAllWindowHandles()
@@ -23,37 +23,21 @@ const scrape = module.exports = (url, sId, pass) => {
     .then(() => driver.findElements(By.name('showGradeInfo')))
     .then(grades => Promise
         .resolve(grades)
-        .reduce((data, grade) => grade.getAttribute('data-bkt')
-            .then(bkt => {
-              data[bkt] = data[bkt] || []
-
-              data[bkt].push(grade)
-
-              return data
-            }), {})
-        .then((data) => Object.keys(data)
-            .reduce((obj, bkt) => {
-              obj[bkt] = obj[bkt].map((link) => {
-                return Promise
-                  .resolve(link.getAttribute('innerHTML'))
-                  .then(score => Object.assign({}, { score: score }))
-                  .tap(driver.executeScript('arguments[0].scrollIntoView()', link))
-                  .tap(driver.wait(until.elementIsVisible(link)))
-                  .tap(link.click())
-                  .tap(driver.wait(until.elementLocated(By.className('gb_heading'))))
-                  .tap(driver.findElement(By.className('gb_heading')))
-                  .tap(driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
-                  .tap(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')).click())
-                  .tap(driver.wait(until.elementIsNotVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
-              })
-              return obj
-            }, data)
-          )
-
-                
-
-        //         }, {})
-        //       return obj
-        //     }, data))
+        .filter(link => link.getAttribute('data-lit')
+            .then(lit => lit == target))
+        .map((link) => {
+          return Promise
+            .resolve(link.getAttribute('innerHTML'))
+            .then(score => Object.assign({}, { score: score }))
+            .tap(driver.executeScript('arguments[0].scrollIntoView()', link))
+            .tap(driver.wait(until.elementIsVisible(link)))
+            .tap(link.click())
+            .tap(driver.wait(until.elementLocated(By.className('gb_heading'))))
+            .then(data => Object.assign(data, { greeting: 'hello' }))
+            .tap(driver.findElement(By.className('gb_heading')))
+            .tap(driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
+            .tap(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')).click())
+            .tap(driver.wait(until.elementIsNotVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
+        })
     )
 }
