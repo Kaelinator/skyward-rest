9654ef0248461c64a35f085a223c0bc1a40072d3
Kaelinator <kirkkael@gmail.com>
2017-12-17 17:16:56 -0600

    Setting up for queries

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const scrape = require('./scrape.js')
 const Promise = require('bluebird')
 
-const skyward = module.exports = 
-    (url) =>
-        (sId, pass, target) => scrape(url, sId, pass, target)
+const skyward = module.exports = (url, sId, pass) => scrape(url, sId, pass)
 
 const pretty = (d) => JSON.stringify(d, null, 2)
 
-skyward('https://skyward.kleinisd.net/')('s592100', 'pis.12345', 'Q2')
-    .then(Promise.all)
-    .then(pretty)
-    .then(console.log)
-    .catch(err => console.log(err))
+skyward('https://skyward.kleinisd.net/', 's531758', 'ASDF;lkj')
+    // .then(Promise.all)
+    // .then(pretty)
+    // .then(console.log)
+    // .catch(err => console.log(err))
--- scrape.js
+++ scrape.js
@@ -1,64 +1,27 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
-const Promise = require('bluebird')
-const parse = require('./parser.js')
+const Promise   = require('bluebird'),
+      puppeteer = require('puppeteer')
 
-const chrome = driver => ({
-  waitForId       : id => driver.wait(until.elementIsVisible(driver.findElement(By.id(id))), 1000),
-  inputCredentials: (u, p) => driver.findElement(By.id('login')).sendKeys(u, Key.TAB, p, Key.RETURN),
-  swap            : w => driver.getAllWindowHandles().then((w) => driver.switchTo().window(w[w.length - 1])),
-  wait            : t => driver.sleep(t),
-  textify         : _ => driver.executeScript('return document.body.innerText')
-})
+const scrape = module.exports = async (url, id, pass) => {
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
 
-const scrape = module.exports = (url, sId, pass, target) => {
+  // page.on('response', p => {
 
-  const driver = new Builder()
-    .forBrowser('chrome')
-    .build()
-  
-  const bot = chrome(driver)
-  
-  return driver.get(url) // ERR: no connection
-    .then(bot.waitForId('login'))
-    .then(bot.waitForId('password'))
-    .then(bot.inputCredentials(sId, pass))
-    .then(bot.wait(1000))
-    .then(bot.textify)
-    .then(res => {
-      if (res.includes('Invalid login or password.'))
-        throw new Error('Invalid login or password.')
-    })
-    .then(bot.swap)
-    .then(() => driver.findElement(By.xpath('//a[@data-nav="sfgradebook001.w"]')).click())
-    .then(() => driver.findElements(By.name('showGradeInfo')))
-    .then(grades => Promise
-        .resolve(grades)
-        .filter(link => link.getAttribute('data-lit')
-            .then(lit => lit == target))
-        .mapSeries((link) =>
-          /* TODO: clean up */
-          Promise
-            .resolve(link.getAttribute('innerHTML'))
-            .then(score => Object.assign({}, { score: score }))
-            .then(data => 
-              driver.executeScript('arguments[0].scrollIntoView()', link)
-                .then(driver.wait(until.elementIsVisible(link)))
-                .then(link.click())
-                .then(() => data))
-            .then(data =>
-              driver.wait(until.elementLocated(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))
-                .then(driver.wait(until.elementLocated(By.css('#gradeInfoDialog div.sf_DialogDataWrap div.sf_DialogData div.sf_DialogHtml'))))
-                .then(driver.wait(until.elementIsVisible(driver.findElement(By.css('#gradeInfoDialog div.sf_DialogDataWrap div.sf_DialogData div.sf_DialogHtml')))))
-                .then(driver.findElements(By.css('#gradeInfoDialog div.sf_DialogDataWrap div.sf_DialogData div.sf_DialogHtml')))
-                .then(bot.textify)
-            .then(html => {return {inner: html, parsed: parse(html, target)}}))
-            .then(data =>
-                driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]'))))
-                .then(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')).click())
-                .then(driver.wait(until.elementIsNotVisible(driver.findElement(By.xpath('//a[@class="sf_DialogClose"][@style="display: block;"]')))))
-                .then(() => data))
-        ).then(x => {
-          driver.quit()
-          return x
-        }))
+  // })
+
+  await page.goto(url)
+  await page.waitFor('#login', { timeout: 10000 })
+  await page.type('#login', id)
+  await page.type('#password', pass)
+  await page.click('#bLogin')
+  await page.waitFor(3000)
+  const [_0, _1, popup] = await browser.pages()
+  const encses = await popup.$('#encses')
+  encses.getProperty('value').then(v => v.jsonValue().then(console.log))
+
+  // await popup.setViewport({ width: 1000, height: 700 })
+  // await popup.waitFor('a[data-nav="sfgradebook001.w"]', { timeout: 10000 })
+  // await popup.click('a[data-nav="sfgradebook001.w"]')
+  // await page.click('#bLogin')
+  // await browser.close()
 }
