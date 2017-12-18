5f1772516ecada65b577a5ab0c20525145bb15b3
Kaelinator <kirkkael@gmail.com>
2017-12-17 20:56:22 -0600

    Grabbing information from login request

--- scrape.js
+++ scrape.js
@@ -1,23 +1,43 @@
 const Promise   = require('bluebird'),
       puppeteer = require('puppeteer')
 
+const handlePopup = async (target) => {
+
+  const popup = await target.page()
+  await popup.waitFor('#encses', { timeout: 10000 })
+  const encses = await popup.$('#encses')
+  encses.getProperty('value').then(v => v.jsonValue())
+}
+
+const handleResponse = async (response) => {
+  const body = await response.buffer()
+  const text = await response.text()
+  console.log({
+    body: body,
+    text: text,
+    headers: response.headers,
+    request: response.request
+  })
+}
+
 const scrape = module.exports = async (url, id, pass) => {
+
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
 
-  // page.on('response', p => {
-
-  // })
-
   await page.goto(url)
-  await page.waitFor('#login', { timeout: 10000 })
+  await page.waitFor('#bLogin', { timeout: 10000 })
+
+  page.on('response', handleResponse)
+  browser.on('targetcreated', handlePopup)
   await page.type('#login', id)
   await page.type('#password', pass)
   await page.click('#bLogin')
-  await page.waitFor(3000)
-  const [_0, _1, popup] = await browser.pages()
-  const encses = await popup.$('#encses')
-  encses.getProperty('value').then(v => v.jsonValue().then(console.log))
+  
+  
+  // const [_0, _1, popup] = await browser.pages()
+  // const encses = await popup.$('#encses')
+  // encses.getProperty('value').then(v => v.jsonValue().then(console.log))
 
   // await popup.setViewport({ width: 1000, height: 700 })
   // await popup.waitFor('a[data-nav="sfgradebook001.w"]', { timeout: 10000 })
