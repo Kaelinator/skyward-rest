44d680742f52a8193db902e5cd256afb50101b95
Kaelinator <kirkkael@gmail.com>
2017-12-27 20:36:49 -0600

    Structured Scraper

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const Promise = require('bluebird')
 
 // const skyward = module.exports = (url, sId, pass) => scrape(url, sId, pass)
 
-const pretty = (d) => JSON.stringify(d, null, 2)
+// const pretty = (d) => JSON.stringify(d, null, 2)
 
 // skyward('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w', 's592100', 'pis.12345')
 // scrape.init('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w', 's531758', 'ASDF;lkj')
+// (async () => {
 
-const check = scrape('testing here')
-check.open()
+Promise.resolve(scrape('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')('s531758', 'ASDF;lkj'))
+	.then(x => x.close())
+// })()
 	// .then(Promise.all)
 	// .then(pretty)
 	// .then(console.log)
--- scrape.js
+++ scrape.js
@@ -1,154 +1,111 @@
 const Promise   = require('bluebird')
 const puppeteer = require('puppeteer')
-const uuid      = require('uuid/v1')
 
-const Scraper = (url) => {
+const Scraper = (url) =>
+  (sId, pass) => {
 
-  const activeScraping = {}
-  return {
+    let loginInfo, encses, popup
+    let retriever
 
-    open: (sId, pass) => {
+    const queue = []
 
-      const id = uuid()
-      const data = {}
+    const referer = url.split('/')
+      .slice(0, -1)
+      .concat('httploader.p?file=sfgradebook001.w')
+      .join('/')
 
-      Promise.resolve(puppeteer.launch())
-        .then(browser => browser.on('targetcreated', handlePopup(id)))
-        .then(browser => browser.newPage())
-        .then(page => page.on('response', handleResponse(id)))
-        .then(page =>
-          Promise.resolve(page.goto(url))
-            .then(() => page.waitForSelector('#bLogin', { timeout: 5000, visibility: true }))
-            .then(() => page.waitFor(150))
-            .then(() => page.type('#login', id))
-            .then(() => page.type('#password', pass))
-            .then(() => page.click('#bLogin')))
-    },
+    const browser = puppeteer.launch()
+      .then(browser => browser.on('targetcreated', handlePopup))
+      .then(browser =>
+        browser.newPage()
+          .then(page =>
+            page.goto(url)
+              .then(() => page.on('response', handleResponse))
+              .then(() => page.waitForSelector('#bLogin', { timeout: 5000, visibility: true }))
+              .then(() => page.waitFor(150))
+              .then(() => page.type('#login', sId))
+              .then(() => page.type('#password', pass))
+              .then(() => page.click('#bLogin'))
+              .then(() => browser)))
 
-    on: (event, callback) => {
+    return {
 
-    },
-
-    handlePopup: (id) =>
-      (target) => {
-
+      retrieve: (callback) => {
+        retriever = callback
       },
 
-    handleResponse: (id) =>
-      (response) => {
-
+      scrape: (lit) => {
+        queue.push(lit)
       },
 
-    query: (id) =>
-      (response, origin, encses, data) => {
-
+      close: () => {
+        browser.close()
+      }
     }
-  }
-}
 
-module.exports = Scraper
-
-const query = (response, origin, encses, data) => {
-
-  const req = new XMLHttpRequest()
-  const info = response
-    .slice(4, -5)
-    .split('^')
-  req.open('POST', origin, false)
-  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-  req.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-  
-  req.send([
-    `action=viewGradeInfoDialog`,
-    `fromHttp=yes`,
-    `stuId=${info[4]}`,
-    `entityId=${data.eid}`,
-    `corNumId=${data.cni}`,
-    `track=${data.trk}`,
-    `section=${data.sec}`,
-    `gbId=${data.gid}`,
-    `bucket=${data.bkt}`,
-    `dialogLevel=1`,
-    `isEoc=${data.eoc}`,
-    `ishttp=true`,
-    `sessionid=${info[1]}%15${info[2]}`,
-    `encses=${encses}`,
-    `dwd=${info[0]}`,
-    `wfaacl=${info[2]}`
-  ].join('&'))
-
-  return req.response 
-}
-
-const scrape = (url, id, pass) => {
-
-  let loginResponse, referer, encses
-
-  const handlePopup = (target) =>
-    Promise.resolve(target.page())
-      .then(popup => {
-        return popup.setViewport({ width: 1000, height: 700 })
+    function handlePopup(target) {
+      [ popup, encses ] = Promise.resolve(target.page())
+        .then(popup => popup.setViewport({ width: 1000, height: 700 })
           .then(() => popup.waitForSelector('#encses', { timeout: 10000 }))
           .then(() => popup.$('#encses'))
           .then(dom => dom.getProperty('value'))
           .then(v => v.jsonValue())
-          .then(encses => {
-            if (encses) {
+          .then(encses => (encses) 
+            ? popup.waitForSelector('a[data-nav="sfgradebook001.w"]', { timeout: 10000 })
+              .then(() => popup.click('a[data-nav="sfgradebook001.w"]'))
+              .then(() => popup.waitForSelector('a[name="showGradeInfo"]'))
+              .then(() => [ popup, encses ])
+            : [ 0, 0 ]))
+    }
 
-              return Promise.resolve(popup.waitForSelector('a[data-nav="sfgradebook001.w"]', { timeout: 10000 }))
-                .then(() => popup.click('a[data-nav="sfgradebook001.w"]'))
-                .then(() => popup.waitForSelector('a[name="showGradeInfo"]'))
-                .then(() => popup.$$eval('a[name="showGradeInfo"]', links => 
-                  links.map(a => ({
+    function handleResponse(response) {
+      loginInfo = response.text()
+        .then((text) =>
+          (text.substring(0, 4) === '<li>') ? text.slice(4, -5).split('^') : 0)
+    }
 
-                  sid: a.getAttribute('data-sid'),
-                  eid: a.getAttribute('data-eid'),
-                  cni: a.getAttribute('data-cni'),
-                  trk: a.getAttribute('data-trk'),
-                  sec: a.getAttribute('data-sec'),
-                  gid: a.getAttribute('data-gid'),
-                  bkt: a.getAttribute('data-bkt'),
-                  lit: a.getAttribute('data-bkt'),
-                  eoc: a.getAttribute('data-eoc')
-                }))))
-                .mapSeries(data => popup.evaluate(query, loginResponse, referer, encses, data))
-            }
-          })
-      })
-      .tap(console.log)
-  
-  const handleResponse = (response) =>
-    Promise.resolve(response.text())
-      .then(text =>
-        Promise.resolve(response.request())
-          .then(req => ({ text, req })))
-      .then(({ text, req }) => {
-        
-        const correct = (text.substring(0, 4) === '<li>')
-        loginResponse = correct ? text : 0
-        referer = correct ? [ 
-          ...req.headers.referer.split('/').slice(0, -1),
-          'httploader.p?file=sfgradebook001.w'
-        ].join('/') : 0
-      })
+    function query(info, origin, encses, data) {
 
-  return Promise.resolve(puppeteer.launch({ headless: false }))
-    .then(browser => browser.on('targetcreated', handlePopup))
-    .then(browser => browser.newPage())
-    .then(page => page.on('response', handleResponse))
-    .then(page => 
-      Promise.resolve(page.goto(url))
-        .then(() => page.waitForSelector('#bLogin', { timeout: 10000, visibility: true }))
-        .then(() => page.waitForSelector('#login', { timeout: 10000, visibility: true }))
-        .then(() => page.waitForSelector('#password', { timeout: 10000, visibility: true }))
-        .then(() => page.waitFor(150))
-        .then(() => page.type('#login', id))
-        .then(() => page.type('#password', pass))
-        .then(() => page.click('#bLogin')))
+      const req = new XMLHttpRequest()
+    
+      req.open('POST', origin, false)
+      req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+      req.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+      
+      req.send([
+        `action=viewGradeInfoDialog`,
+        `fromHttp=yes`,
+        `stuId=${info[4]}`,
+        `entityId=${data.eid}`,
+        `corNumId=${data.cni}`,
+        `track=${data.trk}`,
+        `section=${data.sec}`,
+        `gbId=${data.gid}`,
+        `bucket=${data.bkt}`,
+        `dialogLevel=1`,
+        `isEoc=${data.eoc}`,
+        `ishttp=true`,
+        `sessionid=${info[1]}%15${info[2]}`,
+        `encses=${encses}`,
+        `dwd=${info[0]}`,
+        `wfaacl=${info[2]}`
+      ].join('&'))
+    
+      return req.response
+    }
+  }
 
-  // await popup.setViewport({ width: 1000, height: 700 })
-  // await popup.waitFor('a[data-nav="sfgradebook001.w"]', { timeout: 10000 })
-  // await popup.click('a[data-nav="sfgradebook001.w"]')
-  // await page.click('#bLogin')
-  // await browser.close()
-}
+module.exports = Scraper
+    
+// .then(() => popup.$$eval('a[name="showGradeInfo"]', links => 
+//   links.map(a => ({
+
+//   eid: a.getAttribute('data-eid'),
+//   cni: a.getAttribute('data-cni'),
+//   trk: a.getAttribute('data-trk'),
+//   sec: a.getAttribute('data-sec'),
+//   gid: a.getAttribute('data-gid'),
+//   bkt: a.getAttribute('data-bkt'),
+//   eoc: a.getAttribute('data-eoc')
+// })))
+// .mapSeries(data => popup.evaluate(query, loginInfo, referer, encses, data))
