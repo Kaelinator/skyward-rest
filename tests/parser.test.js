df2b945120563c5418c1757350df9543bd051adc
Kaelinator <kirkkael@gmail.com>
2018-01-08 13:15:37 -0600

    Added meta assignments & properly typed all data

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -1,30 +1,11 @@
 
-const parse       = require('../lib/parser')
-const test        = require('tape')
-// const { compose } = require('../lib/helpers')
+const parse = require('../lib/parser')
+const raw   = require('./raw.data.js')
+const util  = require('util')
 
-const parsed = require('./parsed.data.js')
-const raw    = require('./raw.data.js')
-const util   = require('util')
-
-Object.keys(raw).forEach(lit => {
-  const data = raw[lit].reduce(parse, [])
-  console.log(util.inspect(data, { depth: 7 }))
-})
-
-// const format = compose(
-//   str => `${str}` + 'Hello',
-//   data => JSON.stringify(data)
-// )
-
-// test('Parser', t => {
-
-//   t.plan(Object.keys(raw).length)
-
-//   Object.keys(raw).forEach(lit => {
-
-//     const data = format(raw[lit].reduce(parse, []))
-
-//     t.equal(data, parsed[lit])
-//   })
-// })
+Object.keys(raw)
+  // .filter(lit => lit === 'PR4')
+  .forEach(lit => {
+    const data = raw[lit].reduce(parse, [])
+    console.log(util.inspect(data, { depth: 7, colors: true }))
+  })
