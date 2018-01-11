b997056fba2b3ff1de864979b174d67321e00644
Kaelinator <kirkkael@gmail.com>
2018-01-11 14:44:00 -0600

    Beginning the readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # skyward-rest
-Unofficial Rest API for Skyward
+### Unofficial Rest API for Skyward
+
+```javascript
+const skyward = require('skyward-rest')
+
+const url = 'https://skyward.coolisd.net/...'
+
+skyward(url)(userId, password)
+  .then(student => {
+    student.scrape('PR3')
+      .then(console.log)
+      .then(() => student.scrape('Q2'))
+      .then(console.log)
+      .then(() => student.scrape('S1'))
+      .then(console.log)
+      .then(() => student.close())
+  })
+```
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const scrape  = require('./lib/scrape.js')
-const Promise = require('bluebird')
-const fs      = require('fs')
+const scrape = require('./lib/scrape.js')
+const fs     = require('fs')
 
 const save = data => {
 
@@ -10,23 +9,4 @@ const save = data => {
 	fs.writeFile(path(Date.now()), contents(data), err => { if (err) throw err })
 }
 
-// skyward('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')('s592100', 'pis.12345')
-
-Promise.resolve(scrape('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')('s531758', 'ASDF;lkj'))
-	.then(skyward => {
-		
-		skyward.scrape('Q2')
-			.then(save)
-			.then(() => skyward.scrape('PR4'))
-			.then(save)
-			.then(() => skyward.scrape('PR3'))
-			.then(save)
-			.then(() => skyward.scrape('Q1'))
-			.then(save)
-			.then(() => skyward.scrape('PR2'))
-			.then(save)
-			.then(() => skyward.scrape('PR1'))
-			.then(save)
-			.then(() => skyward.close())
-	})
-
+module.exports = scrape
