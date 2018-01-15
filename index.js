f18632070c2bb9fd9d51d9648653764140836494
Kaelinator <kirkkael@gmail.com>
2018-01-15 15:09:47 -0600

    Unit testing with Jest

--- index.js
+++ index.js
@@ -10,22 +10,10 @@ const save = data => {
 	fs.writeFile(path(Date.now()), contents(data), err => { if (err) throw err })
 }
 
-// skyward('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')('s592100', 'pis.12345')
-
-Promise.resolve(scrape('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')('s531758', 'ASDF;lkj'))
+Promise.resolve(scrape('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')(process.env.SKYUSER, process.env.SKYPASS))
 	.then(skyward => {
 		
-		skyward.scrape('Q2')
-			.then(save)
-			.then(() => skyward.scrape('PR4'))
-			.then(save)
-			.then(() => skyward.scrape('PR3'))
-			.then(save)
-			.then(() => skyward.scrape('Q1'))
-			.then(save)
-			.then(() => skyward.scrape('PR2'))
-			.then(save)
-			.then(() => skyward.scrape('PR1'))
+		skyward.scrape('S1')
 			.then(save)
 			.then(() => skyward.close())
 	})
