ea18c7d3e974117d9634d2ad5eb84336b167d3d7
Kaelinator <kirkkael@gmail.com>
2018-01-27 15:08:45 -0600

    Another restructure

--- index.js
+++ index.js
@@ -2,21 +2,13 @@ const scrape  = require('./lib/scrape.js')
 const Promise = require('bluebird')
 const fs      = require('fs')
 
-const save = data => {
-
-	const contents = (data) => `module.exports = ${JSON.stringify(data)}`
-	const path = (f) => `lib/tests/data/tmp/scrape_${f}.data.js`
-
-	fs.writeFile(path(Date.now()), contents(data), err => { if (err) throw err })
-}
-
-scrape('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')(process.env.SKYUSER, process.env.SKYPASS)
-	.then(skyward => {
+// scrape('https://skyward.kleinisd.net/scripts/wsisa.dll/WService=wsEAplus/seplog01.w')(process.env.SKYUSER, process.env.SKYPASS)
+// 	.then(skyward => {
 		
-		skyward.scrape()
-			.then(save)
-			.then(() => skyward.close())
-	})
-	.catch(console.log.bind(console, 'INDEX:'))
+// 		skyward.scrape()
+// 			.then(save)
+// 			.then(() => skyward.close())
+// 	})
+// 	.catch(console.log.bind(console, 'INDEX:'))
 
 module.exports = scrape
