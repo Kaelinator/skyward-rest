fff07bd9f091958d51a556c7d80c6ce27c05487d
Kaelinator <kirkkael@gmail.com>
2019-03-03 14:15:09 -0600

    gradebook scraper tests

--- src/gradebook/scrape.js
+++ src/gradebook/scrape.js
@@ -1,3 +1,12 @@
-module.exports = ({ dwd, wfaacl, encses }) => {
+const body = ({ dwd, wfaacl, encses }) => {
   if (!dwd || !wfaacl || !encses) throw new TypeError('dwd, wfaacl, & encses are required');
+
+  return `dwd=${dwd}&wfaacl=${wfaacl}&encses=${encses}`;
+};
+
+module.exports = (axios, parse) => (gradebookEndpoint, auth) => {
+  if (!gradebookEndpoint || !auth) throw new TypeError('gradebookEndpoint & auth are required');
+
+  return axios(gradebookEndpoint, body(auth))
+    .then(parse);
 };
--- src/gradebook/scrape.test.js
+++ src/gradebook/scrape.test.js
@@ -1,7 +1,19 @@
 const test = require('tape');
+const Promise = require('bluebird');
 
 const scrape = require('./scrape');
 
-test('scrape', (t) => {
-  t.throws(() => scrape(), /TypeError/, 'given no arguments');
+test('gradebook scrape', (t) => {
+  t.plan(3);
+  t.throws(() => scrape()(), /TypeError/, 'given no arguments');
+
+  const mockAxios = (_, body) => Promise.resolve(body);
+
+  t.throws(() => scrape(mockAxios)('fakeUrl', {}), /TypeError/, 'given no auth data');
+
+  const mockParser = x => x;
+  const auth = { dwd: 1, wfaacl: 2, encses: 3 };
+
+  scrape(mockAxios, mockParser)('fakeUrl', auth)
+    .then(result => t.equal(result, 'dwd=1&wfaacl=2&encses=3'));
 });
