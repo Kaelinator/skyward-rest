1d351a4bf09f017bc6ee55fd8bca1f5b7b153e13
Kaelinator <kirkkael@gmail.com>
2019-03-03 18:15:50 -0600

    parser is working now

--- src/gradebook/parse.js
+++ src/gradebook/parse.js
@@ -1,1 +1,13 @@
-module.exports = ({ data }) => data;
+const cheerio = require('cheerio')
+
+const target = /\$\.extend\(\(sff\.getValue\('sf_gridObjects'\) \|\| {}\), (.*)\)\);/g;
+
+module.exports = ({ data }) => {
+  const $ = cheerio.load(data);
+
+  const script = $('script[data-rel="sff"]').html();
+
+  const results = target.exec(script)
+  
+  return results.length > 1 ? results[1] : {};
+};
