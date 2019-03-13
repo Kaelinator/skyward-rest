2751f2b9aa136ba45d9184289ce7db7d7654a344
Kaelinator <kirkkael@gmail.com>
2019-03-13 13:29:28 -0500

    index scrapes entire year

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ const gradebook = require('./src/gradebook');
 
 const scrape = skywardURL => (user, pass) => authenticate(skywardURL)(user, pass)
   .then(auth => Promise.resolve(reportcard(skywardURL)(auth))
-    .map(({ course, scores }) => gradebook(skywardURL)(auth)(course, scores[0].bucket)));
+    .map(({ course, scores }) => (
+      Promise.resolve(scores)
+        .map(({ bucket }) => gradebook(skywardURL)(auth)(course, bucket)))));
 
 scrape(process.env.SKY_URL)(process.env.SKY_USER, process.env.SKY_PASS)
   .then(data => JSON.stringify(data, null, 2))
-  .then(xml => fs.writeFileAsync('./tmp/output4.json', xml))
+  .then(xml => fs.writeFileAsync('./tmp/output.json', xml))
   .then(() => console.log('done'))
   .catch(console.err);
-
-// scrape(process.env.SKY_URL)(process.env.SKY_USER, process.env.SKY_PASS)
-//   .then(data => JSON.stringify(data, null, 2))
-//   .then(json => fs.writeFileAsync('./tmp/output2.json', json))
-//   .then(() => console.log('done'))
-//   .catch(console.err);
