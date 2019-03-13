fec84d3a18f580c59d537060a962c24d6f79949b
Kaelinator <kirkkael@gmail.com>
2019-03-13 16:07:44 -0500

    cleanup, cleanup

--- src/lib/helpers/main.js
+++
@@ -1,6 +0,0 @@
-
-module.exports.traversers = require('../../data-utils/traversers')
-module.exports.modifiers  = require('../../data-utils/modifiers')
-module.exports.preppers   = require('../../data-utils/preppers')
-module.exports.scrapers   = require('../../data-utils/scrapers')
-module.exports.structures = require('../../data-utils/structures')
