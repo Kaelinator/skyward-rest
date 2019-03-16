0148bd8c99f8be77dad4e8452f0963453e81c989
Kaelinator <kirkkael@gmail.com>
2019-03-16 13:03:35 -0500

    testing

--- index.js
+++ index.js
@@ -5,36 +5,23 @@ Promise.promisifyAll(fs);
 
 require('dotenv').config();
 
-// const reportcard = require('./src/reportcard');
-// const gradebook = require('./src/gradebook');
-// const authenticate = require('./src/authenticate');
-// const history = require('./src/history');
+const reportcard = require('./src/reportcard');
+const gradebook = require('./src/gradebook');
+const authenticate = require('./src/authenticate');
+const history = require('./src/history');
 
-// const scrape = skywardURL => (user, pass) => authenticate(skywardURL)(user, pass)
-//   .then(auth => Promise.resolve(reportcard(skywardURL)(auth))
-//     .map(({ course, scores }) => (
-//       Promise.resolve(scores)
-//         .filter(({ bucket }) => bucket === 'TERM 9')
-//         .map(({ bucket }) => gradebook(skywardURL)(auth)(course, bucket)))));
-
+const scrape = skywardURL => (user, pass) => authenticate(skywardURL)(user, pass)
+  .then(auth => Promise.resolve(reportcard(skywardURL)(auth))
+    .map(({ course, scores }) => (
+      Promise.resolve(scores)
+        .filter(({ bucket }) => bucket === 'TERM 9')
+        .map(({ bucket }) => gradebook(skywardURL)(auth)(course, bucket)))));
 
 // const scrapeHistory = skywardURL => (user, pass) => authenticate(skywardURL)(user, pass)
 //   .then(history(skywardURL));
-const parse = require('./src/reportcard/parse');
-const condense = require('./src/history/condense');
 
-fs.readFileAsync('./tmp/history.html')
-  .then(data => ({ data: data.toString() }))
-  .then(parse)
-  .then(condense)
+scrape(process.env.SKY_URL)(process.env.SKY_USER, process.env.SKY_PASS)
   .then(data => JSON.stringify(data, null, 2))
-  .then(json => fs.writeFileAsync('./tmp/history-condensed.json', json))
+  .then(xml => fs.writeFileAsync('./tmp/output.json', xml))
   .then(() => console.log('done'))
-  .catch(console.log);
-
-// process.env.SKY_USER, process.env.SKY_PASS
-// scrape(process.env.SKY_URL)('***REMOVED***', '***REMOVED***')
-//   .then(data => JSON.stringify(data, null, 2))
-//   .then(xml => fs.writeFileAsync('./tmp/output.json', xml))
-//   .then(() => console.log('done'))
-//   .catch(console.err);
+  .catch(console.err);
